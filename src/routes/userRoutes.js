436b21e6696b47f3cbe525540b20e0a1309fad20
Andrea Saldaña Cavour <andrea.saldana@alumnos.ucn.cl>
2024-11-24 02:48:05 -0300

    added endpoints like: update user and delete user in user routes file

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createUser, getUser } = require('../controllers/userController');
+const { createUser, getUser, getUsers, updateUser, deleteUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/', createUser);
 router.get('/:id', getUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
