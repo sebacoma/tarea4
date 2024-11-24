b28ccd6ac798a5623a12c641ee0e91209e8fabd8
Andrea Saldaña Cavour <andrea.saldana@alumnos.ucn.cl>
2024-11-24 03:26:43 -0300

    add folder and file for authentication

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const SECRET = '12345'; 
+
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, SECRET, { expiresIn: '2m' }); 
+};
+
+// Middleware para validar el token
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'No autenticado.' });
+  }
+
+  jwt.verify(token, SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Token inválido.' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { generateToken, authenticateToken };
