16a1bc35ccca50281d64509f780077f0015b7eb5
Andrea Saldaña Cavour <andrea.saldana@alumnos.ucn.cl>
2024-11-24 03:26:20 -0300

    add jwt functions in routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { createUser, getUser, getUsers, updateUser, deleteUser } = require('../controllers/userController');
+const { authenticateToken } = require('../utils/auth');
 const router = express.Router();
 
 router.post('/', createUser);
-router.get('/:id', getUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.get('/:id', getUser,authenticateToken);
+router.get('/', getUsers,authenticateToken);
+router.put('/:id', updateUser,authenticateToken);
+router.delete('/:id', deleteUser,authenticateToken);
 
 module.exports = router;
